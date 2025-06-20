ebbcc6318c7741b8d3e89b624fde621d444db7f1
GTR-R34-Skyline <shashank.murari@outlook.com>
2025-06-20 23:29:34 +0530

    2nd Commit made

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,64 +1,61 @@
-import sqlImage from '../assets/png/SQL.png';
-import PythonImage from '../assets/png/Python.png';
-import PSImage from '../assets/png/Problem_Solving.png';
-import SEImage from '../assets/png/Software_Engineer.png';
-import sqlbImage from '../assets/png/SQL_Basic.png';
+import img1 from '../assets/png/img1.jpg';
+import img2 from '../assets/png/img2.jpg';
+import img3 from '../assets/png/image-2.png';
+import img4 from '../assets/png/img4.jpg';
+import img5 from '../assets/png/img5.jpg';
+import img6 from '../assets/png/img6.jpg';
+
 
 export const achievementData = {
     bio: "The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. - Alvin Toffler",
     achievements: [
         {
             id: 1,
-            title: 'Introduction to Python',
-            details: 'A certification course on Introduction to Python',
-            date: 'Aug 06 2023',
-            field: 'Coding',
-            image: PythonImage
+            title: 'Behind the Scenes',
+            details: '1st Place in Domain | Event conducted by C2C at SVCE',
+            date: 'Dec 29, 2024',
+            field: 'Event Participation',
+            image: img1
         },
         {
             id: 2,
-            title: 'Introduction to MySQL',
-            details: 'A certification course on Introduction to MySQL',
-            date: 'Aug 14 2023',
-            field: 'Database',
-            image: sqlbImage
+            title: 'Business Reboot',
+            details: '1st Place | Organized by IEEE Student Branch, SVCE',
+            date: 'Oct 17, 2024',
+            field: 'Entrepreneurship',
+            image: img2
         },
         {
             id: 3,
-            title: 'Advancement in  MySQL',
-            details: 'A certification course on MySQL(Intermediate)',
-            date: 'Aug 14 2023',
-            field: 'Database',
-            image: sqlImage
+            title: 'Clash of Minds',
+            details: '2nd Runners Up | Conducted by Speakers’ Forum, SVCE',
+            date: 'Aug 09, 2024',
+            field: 'Public Speaking',
+            image: img3
         },
         {
             id: 4,
-            title: 'Problem Solving Basics',
-            details: 'A certification course on Introduction to Problem Solving',
-            date: 'Sep 10, 2023',
-            field: 'Problem Solving',
-            image: PSImage
+            title: 'SVCE INNOVATES 2024',
+            details: '1st Place | Organized by SVCE for Students’ Research Day',
+            date: 'Apr 16, 2024',
+            field: 'Innovation & Research',
+            image: img4
         },
-         {
+        {
             id: 5,
-            title: 'Software Engineering Internship',
-            details: 'A certification for the training towards Software Engineering ',
-            date: 'Jan 29, 2025',
-            field: 'Software Engineering',
-            image: SEImage
+            title: 'ICMCTT-IV 2024 – Best Paper Presentation',
+            details: 'Best Paper Presentation Award at the Fourth International Conference on Modern Computing Trends and Technology, organized by Kristu Jyoti College and RSP Research Hub',
+            date: 'Nov 28-29, 2024',
+            field: 'Research Presentation',
+            image: img5
+        },
+                {
+            id: 6,
+            title: 'Algorithms Unplugged-Hidden Power Behind Computing',
+            details: 'Exceptional Support and Contribution in organizing the event and the Live Coding Session',
+            date: 'Feb 19, 2025',
+            field: 'Event Support',
+            image: img6
         }
     ]
 };
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below.
-
-/*
-
-export const achievementData = {
-    bio : "",
-    achievements : []
-}
-
-*/
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -6,13 +6,6 @@ export const educationData = [
         startYear: 'October 2023',
         endYear: 'November 2023'
     },
-        {
-        id: 4,
-        institution: 'Coursera in collaboration with Google',
-        course: 'Introduction to Cybersecurity',
-        startYear: 'November 2023',
-        endYear: 'May 2024'
-    },
     {
         id: 2,
         institution: 'Coursera in collaboration with Google',
@@ -26,7 +19,89 @@ export const educationData = [
         course: 'Introduction to Generative AI and LLMs',
         startYear: 'June 2024',
         endYear: 'September 2024'
+    },
+    {
+        id: 4,
+        institution: 'Coursera in collaboration with Google',
+        course: 'Introduction to Cybersecurity',
+        startYear: 'November 2023',
+        endYear: 'May 2024'
+    },
+    {
+        id: 5,
+        institution: 'Coursera + Google',
+        course: 'Google AI Essentials',
+        startYear: 'June 2024',
+        endYear: 'July 2024'
+    },
+    {
+        id: 6,
+        institution: 'HackerRank',
+        course: 'Problem Solving',
+        startYear: 'August 2023',
+        endYear: 'September 2023'
+    },
+    {
+        id: 7,
+        institution: 'HackerRank',
+        course: 'Python Basic',
+        startYear: 'July 2023',
+        endYear: 'August 2023'
+    },
+    {
+        id: 8,
+        institution: 'HackerRank',
+        course: 'SQL-Basic',
+        startYear: 'August 2023',
+        endYear: 'August 2023'
+    },
+    {
+        id: 9,
+        institution: 'HackerRank',
+        course: 'SQL-Intermediate',
+        startYear: 'August 2023',
+        endYear: 'August 2023'
+    },
+    {
+        id: 10,
+        institution: 'Infosys Springboard',
+        course: 'Programming in C',
+        startYear: 'October 2024',
+        endYear: 'December 2024'
+    },
+    {
+        id: 11,
+        institution: 'NIELIT',
+        course: 'Data Science and Machine Learning',
+        startYear: 'August 2023',
+        endYear: 'September 2023'
+    },
+    {
+        id: 12,
+        institution: 'NPTEL',
+        course: 'Blockchain and its application',
+        startYear: 'January 2025',
+        endYear: 'April 2025'
+    },
+    {
+        id: 13,
+        institution: 'Infosys Springboard',
+        course: 'Java Programming Fundamentals',
+        startYear: '15th February 2025',
+        endYear: '22nd February 2025'
+    },
+    {
+        id: 14,
+        institution: 'Infosys Springboard',
+        course: 'Multithreading in Java',
+        startYear: '21st April 2025',
+        endYear: '28th April 2025'
+    },
+    {
+        id: 15,
+        institution: 'Infosys Springboard',
+        course: 'Java I/O with Case Studies',
+        startYear: '26th April 2025',
+        endYear: '3rd May 2025'
     }
-]
-
-
+];
